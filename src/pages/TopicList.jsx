0e06a83532e6e412145b8f4602644f7f3c541945
Jordan VanVeldhuisen <jordanvanveldhuisen@gmail.com>
2024-05-06 14:34:55 -0400

    added a delete feature to the topics view.

--- src/pages/TopicList.jsx
+++ src/pages/TopicList.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import Tooltip from '@mui/material/Tooltip'
 import Box from '@mui/material/Box'
 import { Button } from '@mui/material'
 import "./TopicList.css"
@@ -14,18 +13,76 @@ import globeImage from '../assets/globe 2.png';
 export const TopicList = () => {
   const [topicsAndArticles, setTopicsAndArticles] = useState([])
   const [newTopicName, setNewTopicName] = useState('')
+  const [topics, setTopics] = useState([])
   const articlesContainerRefs = useRef([])
+
+  const CACHE_KEY = 'topicsAndArticles';
+  const CACHE_EXPIRATION_TIME = 10800000; // 3 hours in milliseconds
+
+  const fetchTopicsForUser = async () => {
+    const response = await fetch('http://localhost:8000/topics', {
+        headers: {
+          Authorization: `Token ${JSON.parse(localStorage.getItem('news_token')).token}`,
+        },
+      })
+      if (response.ok) {
+        const topicsArrayForUser = await response.json();
+        // Update your state with the fetched topics and articles
+        setTopics(topicsArrayForUser)
+        console.log(topics)
+      }
+    }
+
+    useEffect(() => {
+        fetchTopicsForUser();
+      }, [])
+
+  const fetchTopicsAndArticles = useCallback(async () => {
+    try {
+      const cachedData = localStorage.getItem(CACHE_KEY);
+      const cachedTimestamp = localStorage.getItem(`${CACHE_KEY}_timestamp`);
+  
+      if (cachedData && cachedTimestamp && Date.now() - cachedTimestamp < CACHE_EXPIRATION_TIME) {
+        // Use the cached data
+        setTopicsAndArticles(JSON.parse(cachedData));
+        return;
+      }
+  
+      // Proceed with the API request if the cache is invalid or expired
+      const response = await fetch('http://localhost:8000/topics/articles', {
+        headers: {
+          Authorization: `Token ${JSON.parse(localStorage.getItem('news_token')).token}`,
+        },
+      });
+  
+      if (response.ok) {
+        const topicsWithArticles = await response.json();
+        // Update your state with the fetched topics and articles
+        setTopicsAndArticles(topicsWithArticles);
+  
+        // Cache the fetched data and the current timestamp
+        localStorage.setItem(CACHE_KEY, JSON.stringify(topicsWithArticles));
+        localStorage.setItem(`${CACHE_KEY}_timestamp`, Date.now());
+      } else {
+        // Handle error
+        console.error('Failed to fetch topics and articles');
+      }
+    } catch (error) {
+      console.error('Error fetching topics and articles:', error);
+    }
+  }, [])
 
   useEffect(() => {
     fetchTopicsAndArticles();
+
     const handleMouseMove = (e, index) => {
         const articlesContainer = articlesContainerRefs.current[index];
         if (articlesContainer && articlesContainer.contains(e.target)) {
-            articlesContainer.scrollLeft += e.deltaY;
-            e.preventDefault(); // Prevent vertical scrolling when over the articles container
+          articlesContainer.scrollLeft += e.deltaY;
+          e.preventDefault(); // Prevent vertical scrolling when over the articles container
         }
-    };
-  
+      };
+    
       const handleMouseLeave = (index) => {
         const articlesContainer = articlesContainerRefs.current[index];
         if (articlesContainer) {
@@ -33,17 +90,21 @@ export const TopicList = () => {
           articlesContainer.removeEventListener('mouseleave', () => handleMouseLeave(index));
         }
       };
-  
-      if (articlesContainerRefs.current) {
-        articlesContainerRefs.current.forEach((ref, index) => {
-          const articlesContainer = ref;
-          if (articlesContainer) {
-            articlesContainer.addEventListener('wheel', (e) => handleMouseMove(e, index));
-            articlesContainer.addEventListener('mouseleave', () => handleMouseLeave(index));
-          }
-        });
-      }
-  
+    
+      const attachEventListeners = () => {
+        if (articlesContainerRefs.current) {
+          articlesContainerRefs.current.forEach((ref, index) => {
+            const articlesContainer = ref;
+            if (articlesContainer) {
+              articlesContainer.addEventListener('wheel', (e) => handleMouseMove(e, index));
+              articlesContainer.addEventListener('mouseleave', () => handleMouseLeave(index));
+            }
+          });
+        }
+      };
+    
+      attachEventListeners();
+    
       return () => {
         if (articlesContainerRefs.current) {
           articlesContainerRefs.current.forEach((ref, index) => {
@@ -55,104 +116,73 @@ export const TopicList = () => {
           });
         }
       };
-    }, []);
+    }, [fetchTopicsAndArticles, articlesContainerRefs])
   
 
-  const handleCreateTopic = async (topicName) => {
-    try {
-      const response = await fetch('http://localhost:8000/topics', {
-        method: "POST",
-          headers: {
-            "Authorization": `Token ${JSON.parse(localStorage.getItem("news_token")).token}`,
-            "Content-Type": "application/json"
-          },
-        body: JSON.stringify({ name: topicName })
-      });
-  
-      if (response.ok) {
-        // Topic created successfully
-        // You can update the state or trigger a re-render here
-        fetchTopicsAndArticles();
-      } else {
-        // Handle error
-        console.error('Failed to create topic');
+    const handleCreateTopic = async (topicName) => {
+        try {
+          const response = await fetch('http://localhost:8000/topics', {
+            method: "POST",
+            headers: {
+              "Authorization": `Token ${JSON.parse(localStorage.getItem("news_token")).token}`,
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: topicName })
+          });
+      
+          if (response.ok) {
+            // Topic created successfully
+            // Invalidate the cache
+            localStorage.removeItem(CACHE_KEY);
+            localStorage.removeItem(`${CACHE_KEY}_timestamp`);
+            fetchTopicsAndArticles();
+            setNewTopicName('')
+          } else {
+            // Handle error
+            console.error('Failed to create topic');
+          }
+        } catch (error) {
+          console.error('Error creating topic:', error);
+        }
       }
-    } catch (error) {
-      console.error('Error creating topic:', error);
-    }
-  }
 
-  const fetchTopicsAndArticles = async () => {
-    try {
-      const response = await fetch('http://localhost:8000/topics/articles', {
-        headers: {
-            Authorization: `Token ${JSON.parse(localStorage.getItem('news_token')).token}`,
-          },
-        });
+      const handleDeleteTopic = async (topicName) => {
+        try {
+            // Find the topic with the matching name
+            const topicToDelete = topics.find((topic) => topic.name === topicName);
+        
+            if (topicToDelete) {
+              const response = await fetch(`http://localhost:8000/topics/${topicToDelete.id}`, {
+                method: "DELETE",
+                headers: {
+                  "Authorization": `Token ${JSON.parse(localStorage.getItem("news_token")).token}`,
+                },
+              });
+        
+              if (response.ok) {
+                // Topic deleted successfully
+                // Invalidate the cache
+                localStorage.removeItem(CACHE_KEY);
+                localStorage.removeItem(`${CACHE_KEY}_timestamp`);
+
+                fetchTopicsForUser(); // Update the topics list after successful deletion
+                fetchTopicsAndArticles(); // Update the topics and articles list after successful deletion
+              } else {
+                // Handle error
+                console.error('Failed to delete topic');
+              }
+            } else {
+              console.error('Topic not found');
+            }
+          } catch (error) {
+            console.error('Error deleting topic:', error);
+          }
+        }
+
   
-      if (response.ok) {
-        const topicsWithArticles = await response.json();
-        // Update your state with the fetched topics and articles
-        setTopicsAndArticles(topicsWithArticles);
-      } else {
-        // Handle error
-        console.error('Failed to fetch topics and articles');
-      }
-    } catch (error) {
-      console.error('Error fetching topics and articles:', error);
-    }
-  }
 
   return (
-    // <div>
-    //   {/* Render topics */}
-    //   {topicsAndArticles.map((topicData, index) => (
-    //     <div key={topicData.topic}>
-    //       <h2>{topicData.topic}</h2>
-    //       <Box
-    //         sx={{ overflowX: 'auto', whiteSpace: 'nowrap', maxHeight: 325 }}
-    //         ref={(el) => (articlesContainerRefs.current[index] = el)}
-    //       >
-    //         {topicData.articles.map((article, articleIndex) => (
-    //           <Card key={articleIndex} sx={{ display: 'inline-block', marginRight: 2, maxWidth: 450, height: 300, }}>
-    //             <CardActionArea>
-    //                 <Box
-    //                     sx={{
-    //                         position: 'relative',
-    //                         width: '100%',
-    //                         paddingBottom: '56.25%', // 16:9 aspect ratio (change as needed)
-    //                     }}
-    //                 >
-    //                     <CardMedia
-    //                         component="img"
-    //                         image={article.url_to_image || '/static/images/cards/contemplative-reptile.jpg'}
-    //                         alt={article.title}
-    //                         sx={{
-    //                         position: 'absolute',
-    //                         top: 0,
-    //                         left: 0,
-    //                         width: '100%',
-    //                         height: '90%',
-    //                         objectFit: 'cover',
-    //                         }}
-    //                     />
-    //                 </Box>
-    //               <CardContent>
-    //                 <Typography gutterBottom variant="h6" component="div" sx={{ fontSize: '0.9rem' }}>
-    //                   {article.source_name}: {article.title}
-    //                 </Typography>
-    //                 <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.8rem' }}>
-    //                   {article.description}
-    //                 </Typography>
-    //               </CardContent>
-    //             </CardActionArea>
-    //           </Card>
-    //         ))}
-    //       </Box>
-    //     </div>
-    //   ))}
     <div style={{ padding: '80px' }}>
-  {/* Render topics */}
   {topicsAndArticles.map((topicData, index) => (
     <div key={topicData.topic}>
       <h2>{topicData.topic}</h2>
@@ -224,6 +254,14 @@ export const TopicList = () => {
           </Card>
         ))}
       </Box>
+      <Button
+            onClick={() => handleDeleteTopic(topicData.topic)}
+            sx={{
+            marginRight: 'auto', // Push the button to the end of the row
+            }}
+        >
+            Delete Topic
+        </Button>
     </div>
   ))}
 
